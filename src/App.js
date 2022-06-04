af738cc9e2379f2314d57219bab3df056494760b
michallangier <m.langier@wp.pl>
2022-06-04 04:03:14 +0200

    uuid generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
 
 function App() {
+  const [longUuid, setLongUuid] = useState('')
+  const [shortUuid, setShortUuid] = useState('')
+
+  function handleClickLong() {
+    setLongUuid(uuidv4())
+  }
+
+  function handleClickShort() {
+    setShortUuid(uuidv4().slice(0, 8))
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>This is simple uuid generator, allows copying to clipboard. Feel free to use it.</div>
+      <div className="grid-container">
+
+        <div className='grid-item'><button onClick={handleClickLong}>Generate long uuid:</button>
+          <CopyToClipboard text={longUuid}>
+            <button>Copy to clipboard</button>
+          </CopyToClipboard>
+        </div>
+
+        <div className='grid-item'><label>{longUuid}</label></div>
+
+        <div className='grid-item'><button onClick={handleClickShort}>Generate short uuid:</button>
+          <CopyToClipboard text={shortUuid}>
+            <button>Copy to clipboard</button>
+          </CopyToClipboard>
+        </div>
+
+        <div className='grid-item'> <label>{shortUuid}</label></div>
+
+      </div>
     </div>
   );
 }
